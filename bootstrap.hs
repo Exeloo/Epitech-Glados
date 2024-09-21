463e6f174d05cc95fc60d0520124cc6059bbd785
Hippolyte lefer <hippolytelefer@gmail.com>
2024-09-21 19:11:08 +0200

    feat: bootstrap of project"

---
+++ bootstrap.hs
@@ -0,0 +1,67 @@
+module Main (main) where
+
+data SExpr = Integer Int | List [SExpr] | Symbol String
+
+instance Show SExpr where
+    show (Integer x) = "Integer : " ++ show x ++ " "
+    show (Symbol x) = "Symbol : " ++ show x ++ " "
+    show (List xs) = "List : [" ++ unwords (map show xs) ++ "]"
+
+getInteger :: SExpr -> Maybe Int
+getInteger (Integer x) = Just x
+getInteger _ = Nothing
+
+getSymbol :: SExpr -> Maybe String
+getSymbol (Symbol x) = Just x
+getSymbol _ = Nothing
+
+getList :: SExpr -> Maybe [SExpr]
+getList (List x) = Just x
+getList _ = Nothing
+
+data Ast = Define {defineKey :: String, defineValue :: Ast} | Call {callFunc :: String, callArgs :: Ast} | Lst [Ast] | Inte Int | Sym String | Boolean Bool deriving Show
+
+sexprToAST :: [String] -> SExpr -> Maybe Ast
+sexprToAST _ (Integer x) = Just (Inte x)
+sexprToAST _ (Symbol x) = Just (Sym x)
+sexprToAST call (List ((Symbol "define"):(Symbol key):value)) =
+    case (mapM (sexprToAST (call++[key])) value) of
+        Just (b:[]) -> Just (Define {defineKey = key, defineValue = b})
+        _ -> Nothing
+sexprToAST call (List ((Symbol s):args)) | s `elem` call = mapM (sexprToAST call) args >>= \lstAst ->
+                    Just (Call {callFunc = s, callArgs = Lst lstAst})
+sexprToAST call (List x) = mapM (sexprToAST call) x >>= \lstAst ->
+                        Just (Lst lstAst)
+
+-- evalAST :: Ast-> Maybe Ast
+-- evalAST (Inte x) = Just (Inte x)
+-- evalAST (Sym x) = Just (Sym x)
+-- evalAST (Define key value) = Just (Define key value)
+-- evalAST (Call func args) =
+-- evalAST (Lst x) = Just (Lst x)
+
+
+main :: IO ()
+main = case sexprToAST ["+", "-", "*", "/"] (List [(Symbol "define"), (Symbol "x"), (List [(Symbol "x"), (Integer 4), (Integer 5)])]) of
+        Just x -> putStrLn (show x)
+        Nothing -> putStrLn "Nothing"
+-- main = case sexprToAST (List [(Symbol "define"), (Symbol "x"), (Integer 5)]) of
+--         Just x -> putStrLn (show x)
+--         Nothing -> putStrLn "Nothing"
+-- main = case sexprToAST (Symbol "here") of
+--         Just x -> putStrLn (show x)
+--         Nothing -> putStrLn "Nothing"
+-- main = putStrLn (show (List [(Symbol "define"), (Symbol "x"), (Integer 5)]))
+-- main = putStrLn (show (List [(Integer 42), (Symbol "Quoi"), (List [(Integer 24), (Symbol "Foo")])]))
+-- main = case getList (List [(Integer 42), (Symbol "Quoi"), (List [(Integer 24), (Symbol "Foo")])]) of
+--         Just x -> putStrLn (show x)
+--         Nothing -> putStrLn "Nothing"
+-- main = case getSymbol (Symbol "Feur") of
+--         Just x -> putStrLn (show x)
+--         Nothing -> putStrLn "Nothing"
+-- main = case getSymbol(Integer 5) of
+--         Just x -> putStrLn (show x)
+--         Nothing -> putStrLn "Nothing"
+-- main = case getInteger (Integer 5) of
+--         Just x -> putStrLn (show x)
+--         Nothing -> putStrLn "Nothing"
