c554006d8218f824fe04e0a752bbd066db6b49f7
Hippolyte lefer <hippolytelefer@gmail.com>
2024-10-02 17:45:04 +0200

    refactor:  callAST and evalAstFunc

--- src/AstEval.hs
+++ src/AstEval.hs
@@ -11,41 +11,29 @@ import Symbol
 import AstData
 import Evaluation
 
-checkListSymbol :: [Ast] -> [[Ast]] -> Either String Bool
-checkListSymbol [] _ = Right False
-checkListSymbol (x:xs) a = case x of
-  ASymbol s -> case checkElemList s a of
-    Right True -> checkListSymbol xs a
-    Right False -> Left ("Symbol " ++ s ++ " not found")
-    Left x' -> Left x'
-  _ -> checkListSymbol xs a
-
-callAST :: Bool -> String -> [Ast] -> [[Ast]] -> Either String Ast
-callAST False f args a = case checkListSymbol args a of
-  Right _ -> callAST True f args a
-  Left x -> Left x
-callAST True "+" [AInt x, AInt y] _ = Right (AInt (x + y))
-callAST True "-" [AInt x, AInt y] _ = Right (AInt (x - y))
-callAST True "*" [AInt x, AInt y] _ = Right (AInt (x * y))
-callAST True "div" [AInt x, AInt y] _ = Right (AInt (x `div` y))
-callAST True "mod" [AInt x, AInt y] _ = Right (AInt (x `mod` y))
-callAST True "eq?" [x, y] _ = Right (ABool (x == y))
-callAST True "<" [AInt x, AInt y] _ = Right (ABool (x < y))
-callAST True "if" [func, a, b] _ = case func of
+callAST :: String -> [Ast] -> Either String Ast
+callAST "+" [AInt x, AInt y] = Right (AInt (x + y))
+callAST "-" [AInt x, AInt y] = Right (AInt (x - y))
+callAST "*" [AInt x, AInt y] = Right (AInt (x * y))
+callAST "div" [AInt x, AInt y] = Right (AInt (x `div` y))
+callAST "mod" [AInt x, AInt y] = Right (AInt (x `mod` y))
+callAST "eq?" [x, y] = Right (ABool (x == y))
+callAST "<" [AInt x, AInt y] = Right (ABool (x < y))
+callAST "if" [func, a, b] = case func of
   ABool True -> Right a
   ABool False -> Right b
   ASymbol "#t" -> Right a
   ASymbol "#f" -> Right b
-  ACall FuncCall { callFunction = FSymbol f, callArgs = arg } -> case callAST True f arg [[]] of
+  ACall FuncCall { callFunction = FSymbol f, callArgs = arg } -> case callAST f arg of
     Right (ABool True) -> Right a
     Right (ABool False) -> Right b
     _ -> Left "Invalid if statement"
   _ -> Left "Invalid if statement"
-callAST _ _ _ _ = Left "Invalid function"
+callAST _ _ = Left "Invalid function"
 
 replaceSymbol :: Symbol -> Ast -> [[Ast]] -> Ast -> Ast
 replaceSymbol _ _ var (ASymbol b)  | checkElemList b var == Right True = case getElemList b var of
-  Right x -> x
+  Right (AAssignation (VarAssignation {assignationKey = _, assignationValue = x})) -> x
   Left _ -> ASymbol b
 replaceSymbol s (a) _ (ASymbol b) | s == b = a
 replaceSymbol s (a) var (ACall FuncCall {callFunction = f, callArgs = args}) = case map (replaceSymbol s (a) var) args  of
@@ -58,7 +46,7 @@ replaceSymbol _ _ _ b = b
 
 evalAstFunc :: [Symbol] -> Ast -> [Ast] -> [[Ast]] -> Either String Ast
 evalAstFunc (arg:args) func (argcall:argcalls) a = evalAstFunc args (replaceSymbol arg argcall a func) argcalls a
-evalAstFunc _ (ACall FuncCall {callFunction = FSymbol f, callArgs = args}) _ a | f `elem` ["+", "-", "*", "div", "mod", "eq?", "<", "if"] = callAST False f args a
+evalAstFunc _ (ACall FuncCall {callFunction = FSymbol f, callArgs = args}) _ _ | f `elem` ["+", "-", "*", "div", "mod", "eq?", "<", "if"] = callAST f args
 evalAstFunc _ (ACall FuncCall {callFunction = FFunc f, callArgs = args}) _ a = evalAST a (ACall FuncCall {callFunction = FFunc f, callArgs = args})
 evalAstFunc _ (AList [body]) [] a = evalAstFunc [] body [] a
 evalAstFunc [] func [] _= Right func
@@ -67,10 +55,10 @@ evalAstFunc [] _ _ _ = Left "Invalid function, too many arguments"
 
 evalAST :: [[Ast]] -> Ast -> Either String Ast
 evalAST _ (AInt x) = Right (AInt x)
-evalAST _ (ASymbol x) = Right (ASymbol x)
+evalAST a (ASymbol x) = Right (replaceSymbol x (ASymbol x) a (ASymbol x))
 evalAST _ (AAssignation var) = Right (AAssignation var)
 evalAST a (ACall FuncCall {callFunction = FSymbol func, callArgs = args})= case mapM (evalAST a) args of
-    Right x -> case callAST False func x a of
+    Right x -> case callAST func x of
         Right x' -> Right x'
         Left x' -> Left x'
     Left x -> Left x
