05c379cc1b1d4da2ba2e00d4209561724c559b1f
Exelo <exelo.corp@gmail.com>
2024-10-07 01:11:50 +0200

    feat: add units tests

--- test/UnitTests/UnitTestsSExprToAst.hs
+++ test/UnitTests/UnitTestsSExprToAst.hs
@@ -10,13 +10,23 @@ assertLeft _ (Left _) = return ()
 assertLeft msg (Right _) = assertFailure $ msg ++ " -> expected failure, but got a Right value"
 
 testSuccessBasic :: Test
-testSuccessBasic = TestCase $ assertEqual "Test add" (Right (ACall FuncCall {callFunction = FSymbol "+", callArgs = [AInt 1, AInt 2]})) (sExpToAst (SList [SSymbol "+", SInt 1, SInt 2]))
+testSuccessBasic = TestCase $ assertEqual "STOA Test add" (Right (ACall FuncCall {callFunction = FSymbol "+", callArgs = [AInt 1, AInt 2]})) (sExpToAst (SList [SSymbol "+", SInt 1, SInt 2]))
 testErrorNoInstruction :: Test
-testErrorNoInstruction = TestCase $ assertLeft "Invalid lisp (first value must be an array)" (sExpToAst (SInt 42))
+testErrorNoInstruction = TestCase $ assertLeft "STOA Invalid lisp (first value must be an array)" (sExpToAst (SInt 42))
+
+testSTOALambda :: Test
+testSTOALambda = TestCase $ assertEqual "STOA Test Lambda" (Right (ACall FuncCall {callFunction = FFunc FuncDeclaration {declareArgs = ["a", "b"], declareBody = [ACall FuncCall {callFunction = FSymbol "+", callArgs = [ASymbol "a", ASymbol "b"]}]}, callArgs = [AInt 3, AInt 4]})) (sExpToAst (SList [SList [SSymbol "lambda", SList [SSymbol "a", SSymbol "b"], SList [SSymbol "+", SSymbol "a", SSymbol "b"]], SInt 3, SInt 4]))
+testSTOADefine :: Test
+testSTOADefine = TestCase $ assertEqual "STOA Test Define" (Right (AAssignation (VarAssignation "x" (AInt 5)))) (sExpToAst (SList [SSymbol "define", SSymbol "x", SInt 5]))
+testSTOAAdd :: Test
+testSTOAAdd = TestCase $ assertEqual "STOA Test Add" (Right (ACall FuncCall {callFunction = FSymbol "+", callArgs = [AInt 10, AInt 5]})) (sExpToAst (SList [SSymbol "+", SInt 10, SInt 5]))
 
 testListSExprToAst :: Test
 testListSExprToAst =
   TestList [
     TestLabel "testSuccessBasic" testSuccessBasic,
-    TestLabel "testErrorNoInstruction" testErrorNoInstruction
+    TestLabel "testErrorNoInstruction" testErrorNoInstruction,
+    TestLabel "testSTOALambda" testSTOALambda,
+    TestLabel "testSTOADefine" testSTOADefine,
+    TestLabel "testSTOAAdd" testSTOAAdd
   ]
