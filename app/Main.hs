08e304093be3d6c9fcca9436754810d9b25d698f
Robin Biard <robin.biard@epitech.eu>
2024-10-22 18:44:12 +0200

    feat(help): add helper in main function

--- app/Main.hs
+++ app/Main.hs
@@ -5,10 +5,17 @@ import Launch
 import System.Exit
 import System.Environment
 
+help :: IO ()
+help = putStrLn "USAGE: ./glados [file/line]\n" >>
+       putStrLn "       file       path of the file used" >>
+       putStrLn "       line       line of code used" >>
+       exitSuccess
+
+
 main :: IO ()
 main = getArgs >>= \args ->
     if "--help" `elem` args
-        then exitSuccess
+        then help
         else launch args >>= \res ->
             if res
                 then exitSuccess
