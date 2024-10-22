6a43b0e587a3c981d117627eab0152a616223984
Robin Biard <robin.biard@epitech.eu>
2024-10-23 00:41:36 +0200

    refactor(SExpr): change SExpr data to match our own language

--- src/SExprData.hs
+++ src/SExprData.hs
@@ -11,21 +11,36 @@ import Symbol
 
 data SExpr =
    SInt Int |
-   SList [SExpr] |
+   SFloat Float |
+   SBool Bool |
+   SString String |
    SSymbol Symbol |
-   SString String
+   SArray [SExpr] |
+   SStruct [(Symbol, SExpr)] |
+   SList [SExpr] |
+   Undefined
 
 instance Show SExpr where
   show (SInt x) = "Integer: " ++ show x
-  show (SSymbol x) = "Symbol: " ++ show x
-  show (SList x) = "List: [" ++ foldl (\a b -> a ++ (if null a then "" else ", ") ++ show b) [] x ++ "]"
+  show (SFloat x) = "Float: " ++ show x
+  show (SBool x) = "Boolean: " ++ show x
   show (SString x) = "String: \"" ++ x ++ "\""
+  show (SSymbol x) = "Symbol: " ++ show x
+  show (SArray x) = "Array: [" ++ foldl (\a b -> a ++ (if null a then "" else ", ") ++ show b) [] x ++ "]"
+  show (SStruct x) = "Struct: {" ++ foldl (\a (b, c) -> a ++ (if null a then "" else ", ") ++ show b ++ ": " ++ show c) [] x ++ "}"
+  show (SList x) = "List: [" ++ foldl (\a b -> a ++ (if null a then "" else ", ") ++ show b) [] x ++ "]"
+  show Undefined = "Undefined"
 
 instance Eq SExpr where
   (SInt x1) == (SInt x2) = x1 == x2
-  (SSymbol s1) == (SSymbol s2) = s1 == s2
-  (SList l1) == (SList l2) = l1 == l2
+  (SFloat f1) == (SFloat f2) = f1 == f2
+  (SBool b1) == (SBool b2) = b1 == b2
   (SString str1) == (SString str2) = str1 == str2
+  (SSymbol s1) == (SSymbol s2) = s1 == s2
+  (SArray a1) == (SArray a2) = a1 == a2
+  (SStruct struct1) == (SStruct struct2) = struct1 == struct2
+  (SList l1) == (SList l2) = l1 == l2
+  Undefined == Undefined = True
   _ == _ = False
 
 
