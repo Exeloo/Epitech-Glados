ab2ad461357d1183f14c6e143370c1e1a7a9674a
Tchips46tek <leo.outmizguine@epitech.eu>
2024-10-29 02:06:06 +0100

    feat(vm): example of factorial

--- src/executer/Launch.hs
+++ src/executer/Launch.hs
@@ -5,16 +5,28 @@ import ExecInstructions (exec)
 import System.IO (hPutStrLn, stderr)
 import System.Directory (doesFileExist)
 import Text.Megaparsec
+import SExprToInstructions (sExprToInsts)
+import InstructionData (Insts)
 
-execFile :: String -> IO Bool
-execFile file =
-    case parse parseSExpr "" file of
-        Left err -> putStrLn ("Parse error: " ++ errorBundlePretty err) >> return False
+
+execFile :: Insts -> IO Bool
+execFile insts =
+    case exec insts [] insts [] of
+        Left err -> hPutStrLn stderr ("Exec error: " ++ err) >> return False
         Right res -> print res >> return True
+
+parseFile :: String -> IO Bool
+parseFile file =
+    case parse parseSExpr "" file of
+        Left err -> hPutStrLn stderr ("Parse error: " ++ errorBundlePretty err) >> return False
+        Right res ->
+            case sExprToInsts res of
+                Left err -> hPutStrLn stderr ("Parse error: " ++ err) >> return False
+                Right insts -> execFile insts
 
 launch :: [String] -> IO Bool
 launch [path] = doesFileExist path >>= \exist ->
     if exist
-        then readFile path >>= \file -> execFile file
+        then readFile path >>= \file -> parseFile file
         else putStrLn ("File" ++ path ++ "do not exist") >> return False
 launch _ = hPutStrLn stderr "Invalid args" >> return False
