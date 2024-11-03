0468fe83b3bac7dd1c4955ff9724b4d3e9b417c1
Deniz Uzuntok Husson <deniz.uzuntok-husson@epitech.eu>
2024-11-03 15:22:34 +0100

    feat(tests): add units tests for Datas

---
+++ executer/test/UnitTests/UnitTestsInstructionData.hs
@@ -0,0 +1,21 @@
+module UnitTests.UnitTestsInstructionData(testListInstructionData) where
+
+import Test.HUnit
+import InstructionData
+
+testShowSysCall :: Test
+testShowSysCall = TestCase $ assertEqual "test show a syscall" ("Add") (show (Add))
+
+testShowInstructionData :: Test
+testShowInstructionData = TestCase $ assertEqual "test show an instruction data" ("Call") (show (Call))
+testEqInstructionData :: Test
+testEqInstructionData = TestCase $ assertEqual "test show an instruction data" (True) ((Ret) == (Ret))
+
+
+testListInstructionData :: Test
+testListInstructionData =
+    TestList [
+        TestLabel "testShowSysCall" testShowSysCall,
+        TestLabel "testShowInstructionData" testShowInstructionData,
+        TestLabel "testEqInstructionData" testEqInstructionData
+    ]
