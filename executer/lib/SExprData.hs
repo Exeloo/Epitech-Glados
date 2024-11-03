e86e22ade5eeb7e5054d1c24814e639efb948a55
Tchips46tek <leo.outmizguine@epitech.eu>
2024-11-03 12:51:18 +0100

    feat(vm): access and modify types, add modulo

--- executer/lib/SExprData.hs
+++ executer/lib/SExprData.hs
@@ -1,6 +1,6 @@
 module SExprData (SSysCall(..), SValue(..), SInst(..), SAsm(..), SExpr) where
 
-data SSysCall = SAdd | SSub | SMul | SDiv | SEq | SLess | SNot | SOr | SAnd | SAccessArray | SModifyArray | SAccessObject | SModifyObject | SPrint
+data SSysCall = SAdd | SSub | SMul | SDiv | SMod | SEq | SLess | SNot | SOr | SAnd | SAccess | SModify | SPrint
     deriving (Show, Eq)
 
 data SValue = SInt Int | SBool Bool | SDouble Double | SString String | SArray [SValue] | SObject [(String, SValue)] | SValueCall SSysCall | SUndefined
