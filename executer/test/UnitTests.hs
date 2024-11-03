191174a25a3d8f3cc03654ede0fe442d30e84b28
Deniz Uzuntok Husson <deniz.uzuntok-husson@epitech.eu>
2024-11-03 15:25:05 +0100

    feat(tests): add new Data tests in run

--- executer/test/UnitTests.hs
+++ executer/test/UnitTests.hs
@@ -1,7 +1,13 @@
 import Test.HUnit (runTestTT)
 import UnitTests.UnitTestsSExprToInstruction
+import UnitTests.UnitTestsExecInstruction
+import UnitTests.UnitTestsInstructionData
+import UnitTests.UnitTestsSExprData
 
 main :: IO ()
 main = do
     _ <- runTestTT testListSExprToInstruction
+    _ <- runTestTT testListExecInstruction
+    _ <- runTestTT testListInstructionData
+    _ <- runTestTT testListSExprData
     return ()
