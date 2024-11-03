1ef837c041fc8dad5951bd8ca6b031864cc24446
Hippolyte lefer <hippolytelefer@gmail.com>
2024-11-03 15:00:46 +0100

    feat: enhance sexpr to ast unit test

--- compiler/lib/AstData.hs
+++ compiler/lib/AstData.hs
@@ -45,10 +45,18 @@ data Ast =
 
 instance Eq Ast where
   (AInt x) == (AInt y) = x == y
+  (AFloat x) == (AFloat y) = x == y
   (ABool x) == (ABool y) = x == y
   (ASymbol x) == (ASymbol y) = x == y
   (AString x) == (AString y) = x == y
   (AList xs) == (AList ys) = xs == ys
+  (AAssignation (VarAssignation x y)) == (AAssignation (VarAssignation x' y')) = x == x' && y == y'
+  (ADeclaration (FuncDeclaration x y)) == (ADeclaration (FuncDeclaration x' y')) = x == x' && y == y'
+  (ACall (FuncCall x y)) == (ACall (FuncCall x' y')) = x == x' && y == y'
+  (ACall (ArrayAccess x y)) == (ACall (ArrayAccess x' y')) = x == x' && y == y'
+  (ALoop (ForLoop x y z w)) == (ALoop (ForLoop x' y' z' w')) = x == x' && y == y' && z == z' && w == w'
+  (ALoop (WhileLoop x y)) == (ALoop (WhileLoop x' y')) = x == x' && y == y'
+  (AObject x) == (AObject y) = x == y
   _ == _ = False
 
 instance Show Ast where
