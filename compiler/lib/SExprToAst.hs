9e02a6453333f11ff4a599dcf4258acdceca89d7
Hippolyte lefer <hippolytelefer@gmail.com>
2024-11-03 11:55:28 +0100

    feat: support continue and break statements and basic built-in function

--- compiler/lib/SExprToAst.hs
+++ compiler/lib/SExprToAst.hs
@@ -125,3 +125,5 @@ sExpInstructionToAst x = Left $ "Invalid instruction: " ++ show x
 sExpToAst :: SExpr -> Either String Ast
 sExpToAst (SLine xs) = sExpInstructionToAst xs
 sExpToAst x = Left $ "Invalid SExpr: " ++ show x
+
+-- sExpInstructionToAst [SIInt 1, SISymbol "+", SIInt 2, SISymbol "*", SIInt 3]
