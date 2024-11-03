210dee6f5494fa14a30f2705ec584ab0307911d2
Exelo <exelo.corp@gmail.com>
2024-11-03 07:48:19 +0100

    feat(compiler): add bytecode declaration parser

---
+++ compiler/lib/bytecode/ParseDeclaration.hs
@@ -0,0 +1,22 @@
+{-
+-- EPITECH PROJECT, 2024
+-- B-FUN-500_glados
+-- File description:
+-- ParseDeclaration
+-}
+
+module ParseDeclaration (parseDeclaration) where
+
+import AstData
+import BytcodeOperations
+import BytecodeTypes
+
+import ScopeUtils (hasVarInScopes, addVarToScope)
+
+parseDeclaration :: AstDeclaration -> String -> BytecodeParams -> PBResult
+parseDeclaration dec name params =
+  if hasVarInScopes name params
+  then (nParams, Right "")
+  else (params, Left "Invalid declaration name: \"" ++ name ++ "\" already exist.")
+    where
+      nParams = addVarToScope (name, (ADeclaration dec)) params
